6ff3f793ffed922c06c7e188898f6080cc99a56c
Aleksey Tsalolikhin <aleksey@verticalsysadmin.com>
2012-08-23 19:17:32 -0700

    added a local .vimrc file

--- examples/verticalsysadmin_training_examples/run_slides.sh
+++ examples/verticalsysadmin_training_examples/run_slides.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-# Purpose: show all my cfengine examples using vim
+# Purpose: show all my cfengine examples using vim.  this allows me to edit them during the presentation.
+
+# note: there is a .vimrc in this directory.
+# you have to have "set exrc" enabled for vim to load it automatically
 
 for f in `ls -1`
 do
