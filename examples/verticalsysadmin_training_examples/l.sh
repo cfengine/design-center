db163e28efa46209c5d210fd4ecad31659a1b6a1
Aleksey Tsalolikhin <aleksey@verticalsysadmin.com>
2012-09-23 13:54:51 -0700

    added chapters 300 and 310

--- examples/verticalsysadmin_training_examples/l.sh
+++ examples/verticalsysadmin_training_examples/l.sh
@@ -1,3 +1,5 @@
 # colorize "ls" output
-ls -1| perl -e '$oldchapter=0; while (<STDIN>) { $chapter = substr($_,0,3);  if ($chapter != $oldchapter) { print "$chapter\n"; $oldchapter = $chapter; };  print; }' | \
-remark ./VSAcf3.remark | less -r
+ls -1| \
+  perl -e '$oldchapter=0; while (<STDIN>) { /^(\d\d\d)/; $chapter = $1;  if ($chapter != $oldchapter) { print "$chapter\n"; $oldchapter = $chapter; };  print; }; $purpose_of_this_script = "insert chapter breaks based on the first three digits in each line.   input: 010-... 010-..... 020-.... 020-.... etc.  insert the chapter break between 010 and 020.";' | \
+  remark ./VSAcf3.remark | \
+  less -r  # the -r switch preserves the colorization codes
