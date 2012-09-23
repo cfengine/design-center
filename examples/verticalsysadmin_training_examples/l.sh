b79d5c4f68f05c45574e139c6ede8ad20c230cfc
Aleksey Tsalolikhin <aleksey@verticalsysadmin.com>
2012-09-23 13:25:30 -0700

    automatically insert chapter breaks

--- examples/verticalsysadmin_training_examples/l.sh
+++ examples/verticalsysadmin_training_examples/l.sh
@@ -1,2 +1,3 @@
 # colorize "ls" output
-ls -1| remark ./VSAcf3.remark |less -r
+ls -1| perl -e '$oldchapter=0; while (<STDIN>) { $chapter = substr($_,0,3);  if ($chapter != $oldchapter) { print "$chapter\n"; $oldchapter = $chapter; };  print; }' | \
+remark ./VSAcf3.remark | less -r
