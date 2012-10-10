c5860f76633ec1053757964023cd15b925a29fe8
Diego Zamboni <diego@zzamboni.org>
2012-10-10 02:25:03 -0500

    Fixed typos, reduced spurious output and dead code.

--- tools/cf-sketch/perl-lib/DesignCenter/System.pm
+++ tools/cf-sketch/perl-lib/DesignCenter/System.pm
@@ -74,17 +74,12 @@ sub list
     foreach my $repo (@{DesignCenter::Config->repolist}) {
       my @sketches = $self->list_internal($repo, $terms);
 
-      my $contents = CFSketch::CFSketch::repo_get_contents($repo);
+      my $contents = CFSketch::repo_get_contents($repo);
 
+      if (!scalar @sketches) {
+        return $res;
+      }
       foreach my $sketch (@sketches) {
-        # # this format is easy to parse, even if the directory has spaces,
-        # # because the first two fields won't have spaces
-        # my @docs = grep {
-        #   $contents->{$sketch}->{manifest}->{$_}->{documentation}
-        # } sort keys %{$contents->{$sketch}->{manifest}};
-
-        # print GREEN, "$sketch", RESET, " $contents->{$sketch}->{fulldir}\n";
-
         # Create new Sketch object
         $res->{$sketch} = DesignCenter::Sketch->new(name => $sketch, %{$contents->{$sketch}});
         # Set installed to its full install location
@@ -120,7 +115,7 @@ sub list_internal
     print "Looking for terms [@$terms] in cf-sketch repository [$repo]\n"
       if DesignCenter::Config->verbose;
 
-    my $contents = CFSketch::CFSketch::repo_get_contents($repo);
+    my $contents = CFSketch::repo_get_contents($repo);
     print "Inspecting repo contents: ", DesignCenter::JSON->coder->encode($contents), "\n"
       if DesignCenter::Config->verbose;
 
@@ -144,7 +139,7 @@ sub activations {
   unless (defined $activations && ref $activations eq 'HASH') {
     Util::error "Can't load any activations from ".DesignCenter::Config->actfile."\n"
         if DesignCenter::Config->verbose;
-    Util::error "There are no configured sketches.\n";
+#    Util::error "There are no configured sketches.\n";
     return undef;
   }
   return $activations;
