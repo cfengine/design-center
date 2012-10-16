fb8af3eddb34072d61c347f7ace9c9a412686f81
Ted Zlatanov <tzz@lifelogs.com>
2012-10-16 17:01:28 -0400

    vagrant/splunk: Vagrant integration for Splunk install

--- tools/test/vagrant/test_dc.sh
+++ tools/test/vagrant/test_dc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DC=$1
+EXTRA=$2
 
 # If $DC does not exist, do nothing
 test -d $DC || exit 1
@@ -8,4 +9,4 @@ test -d $DC || exit 1
 apt-get -qq -y install make
 cd $DC/tools/test/
 export PATH=/var/cfengine/bin:${PATH}
-make splunk
+#make splunk
