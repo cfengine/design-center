531373ae4b3bb0793c0a35cda623f48a0b83de56
Ted Zlatanov <tzz@lifelogs.com>
2013-01-29 15:57:25 -0500

    cf-sketch: new cfsketches format and a Makefile+Perl script to generate it

---
+++ sketches/regenerate.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use JSON;
+use File::Basename;
+use File::Slurp;
+use File::Find;
+
+my @todo;
+find(\&wanted, '.');
+
+sub wanted
+{
+ push @todo, $File::Find::name if $_ eq 'sketch.json';
+}
+
+foreach my $f (@todo)
+{
+ my $d = dirname($f);
+ $d =~ s,^\./,,;
+ print $d, " ";
+ my $j = read_file($f);
+ my $out = JSON->new()->relaxed()->utf8()->allow_nonref()->decode($j);
+ print JSON->new()->utf8()->canonical()->encode($out), "\n";
+}
