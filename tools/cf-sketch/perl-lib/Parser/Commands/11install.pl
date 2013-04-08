677c63c19761ea8c80f2ab47bbc4a79c84f7aaaa
Diego Zamboni <diego@zzamboni.org>
2013-04-08 02:14:32 -0500

    Reformatted.

--- tools/cf-sketch/perl-lib/Parser/Commands/11install.pl
+++ tools/cf-sketch/perl-lib/Parser/Commands/11install.pl
@@ -1,4 +1,4 @@
-# Time-stamp: <2012-10-08 11:25:54 a10022>
+# Time-stamp: <2013-04-08 02:14:09 a10022>
 #
 # search command for searching through sketch list
 # Diego Zamboni, October 1st, 2012.
@@ -22,25 +22,11 @@ use DesignCenter::Config;
 
 ######################################################################
 
-sub command_install {
-  my $sketchstr = shift;
-  my $sketches = [ split /[,\s]+/, $sketchstr ];
-  DesignCenter::Config->_repository->install($sketches)
+sub command_install
+{
+    my $sketchstr = shift;
+    my $sketches = [ split /[,\s]+/, $sketchstr ];
+    DesignCenter::Config->_repository->install($sketches)
 }
-
-# sub command_search {
-#   my $regex=shift;
-#   my $err = Util::check_regex($regex);
-#   if ($err) {
-#     Util::error($err);
-#   } else {
-#     @res = $Config{_repository}->list($regex eq 'all' ? "." : $regex);
-#     foreach my $found (@res) {
-#       print GREEN, $found->name, RESET, " ".$found->location."\n";
-#     }
-
-#   }
-# }
-
 
 1;
