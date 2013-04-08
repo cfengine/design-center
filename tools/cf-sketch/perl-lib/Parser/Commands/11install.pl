d446bb5ed8796f7c44c13749610970529630ebe9
Diego Zamboni <diego@zzamboni.org>
2013-04-08 03:12:00 -0500

    Updated install command for DC API.

--- tools/cf-sketch/perl-lib/Parser/Commands/11install.pl
+++ tools/cf-sketch/perl-lib/Parser/Commands/11install.pl
@@ -1,12 +1,10 @@
-# Time-stamp: <2013-04-08 02:14:09 a10022>
+# Time-stamp: <2013-04-08 03:07:17 a10022>
 #
 # search command for searching through sketch list
 # Diego Zamboni, October 1st, 2012.
 # diego.zamboni@cfengine.com
 
 use Term::ANSIColor qw(:constants);
-
-use DesignCenter::Config;
 
 ######################################################################
 
@@ -23,10 +21,26 @@ use DesignCenter::Config;
 ######################################################################
 
 sub command_install
-{
+  {
     my $sketchstr = shift;
-    my $sketches = [ split /[,\s]+/, $sketchstr ];
-    DesignCenter::Config->_repository->install($sketches)
-}
+    my @sketches = split /[,\s]+/, $sketchstr;
+    my @todo = map
+      {
+        {
+          sketch => $_, force => $Config{force}, source => $Config{sourcedir},
+            target => $Config{repolist}->[0],
+          }
+      } @sketches;
+    my ($success, $result) = main::api_interaction({install => \@todo});
+    Util::print_api_messages($result);
+    my $installresult = Util::hashref_search($result, qw/data install/);
+    if (ref $installresult eq 'HASH') {
+      foreach my $repo (sort keys %$installresult) {
+        foreach my $sketch (sort keys %{$installresult->{$repo}}) {
+          Util::success("Sketch $sketch installed under $repo.\n");
+        }
+      }
+    }
+  }
 
 1;
