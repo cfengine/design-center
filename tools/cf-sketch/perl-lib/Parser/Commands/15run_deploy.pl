1ef118207b5149d6f012170f59abf88b9980f4bc
Diego Zamboni <diego@zzamboni.org>
2013-04-10 02:52:39 -0500

    Added 'generate' and 'deploy' commands.
    For now both are the same - the type of runfile generate (standalone or
    non-standalone) is controlled by the API configuration.

---
+++ tools/cf-sketch/perl-lib/Parser/Commands/15run_deploy.pl
@@ -0,0 +1,63 @@
+#
+# run and deploy: generate runfile, run or integrate it
+#
+# CFEngine AS, October 2012
+
+use Term::ANSIColor qw(:constants);
+
+use Util;
+
+######################################################################
+
+%COMMANDS =
+  (
+   # 'run' =>
+   # [
+   #  [
+   #   'run [ARGUMENTS]',
+   #   'Execute the currently-activated sketches immediately using cf-agent. If any arguments are given, they are passed as-is to cf-agent. Remember to use -K if you want all promises to be evaluated (i.e. when testing in quick sequence).)',
+   #   '(.+)?',
+   #  ],
+   # ],
+   'deploy' =>
+   [
+    [
+     'deploy',
+     'Install the currently-activated sketches for automatic execution from promises.cf',
+     '',
+    ],
+   ],
+   'generate' =>
+   [
+    [
+     '-generate [-s] [FILE]',
+     'Generate a runfile for all the currently-activated sketches. '.
+     'If -s is used [NOT YET IMPLEMENTED], a standalone runfile is generated (with its own "body common control").',
+     '(?:(-s)\b\s*)?(.*)',
+    ]
+   ],
+  );
+
+######################################################################
+
+
+# sub command_run {
+#   my $opts=shift || "";
+#   my $file = DesignCenter::Config->_system->generate_runfile;
+#   my $command = CFSketch::cfengine_binary('cf-agent') . " $opts -f $file";
+#   Util::output(GREEN."\nNow executing the runfile with: $command\n\n".RESET);
+#   system($command);
+# }
+
+sub command_generate {
+    my ($success, $result) = main::api_interaction({regenerate => 1});
+    return unless $success;
+    Util::success("Runfile successfully generated.");
+}
+
+sub command_deploy {
+    # For now they are synonymous
+    command_generate;
+}
+
+1;
