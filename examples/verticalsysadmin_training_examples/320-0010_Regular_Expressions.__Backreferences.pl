fd7ee064c0f10431ebb19dc5a30930f1ca8ca5b7
Aleksey Tsalolikhin <aleksey@verticalsysadmin.com>
2013-05-02 22:06:18 -0400

    update at close of 4 day cfengine training in New Brunswick

--- examples/verticalsysadmin_training_examples/320-0010_Regular_Expressions.__Backreferences.pl
+++ examples/verticalsysadmin_training_examples/320-0010_Regular_Expressions.__Backreferences.pl
@@ -4,16 +4,13 @@
 # match "jumped" and "dog."
 
 
-$_ = "The quick brown fox jumped over the lazy dog.";
+$record =
+"James Alexander Richard Smith";
 
-if ( / (j.*d) .* (.*)$/ ) {
+if ( $record =~ /^(.*?) .* (.*)$/ ) {
 
-  print "Found a match\n";
+  print "First name: $1\n";
 
-  print "The line being examined was: $0\n";
-
-  print "The first capture buffer contains: $1\n";
-
-  print "The 2nd capture buffer contains: $2\n";
+  print "Last name: $2\n";
 }
 
