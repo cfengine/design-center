85dcf95d82c30e0b318ae1f1f5ee4386a6b3ac3c
Aleksey Tsalolikhin <aleksey@verticalsysadmin.com>
2013-05-07 10:35:05 -0500

    add changes made during day 2 of Bloomington 2013 training

--- examples/verticalsysadmin_training_examples/320-0010_Regular_Expressions.__Backreferences.pl
+++ examples/verticalsysadmin_training_examples/320-0010_Regular_Expressions.__Backreferences.pl
@@ -1,16 +1,14 @@
 #!/usr/bin/perl
 
 
-# match "jumped" and "dog."
-
 
 $record =
 "James Alexander Richard Smith";
 
-if ( $record =~ /^(.*?) .* (.*)$/ ) {
+if ( $record =~ /^(.*?) (.*) (.*)$/ ) {
 
   print "First name: $1\n";
-
-  print "Last name: $2\n";
+  print "Middle name(s): $2\n";
+  print "Last name: $3\n";
 }
 
