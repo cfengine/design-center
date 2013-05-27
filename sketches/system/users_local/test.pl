4b537ba397e46b87b7ef4e3554b4637f53d65a86
Ted Zlatanov <tzz@lifelogs.com>
2013-05-27 10:26:05 -0400

    users_local: implement local file editing and runbased (groupadd/useradd) user management
    (cherry picked from commit 805cf689612eeb34bbf6fa0411f542d1da34fcaa)

--- sketches/system/users_local/test.pl
+++ sketches/system/users_local/test.pl
@@ -7,11 +7,16 @@ use lib "../../libraries/dclib";
 use dctest;
 
 my $todo = {
-            "metadata check" => qr/R: cfdc_users:local: Users::Local/,
-            "test user info1" => qr|Requested user tzz with GECOS 'Ted. Zlatanov' and UID/GID '10000/20000', living in './home./tzz' with shell './bin./bash' and password hash 'mypass'.  The group name is mygroup.|,
-            "test user info2" => qr|pwentry for tzz is 'tzz:x:10000:20000:Ted. Zlatanov:./home./tzz:./bin./bash|,
+            "metadata check1" => qr/R: cfdc_users:local: Users::Local/,
+            "metadata check2" => qr/R: cfdc_users:runbased: Users::Local/,
+            "test user info1" => qr|Requested user tzz with GECOS 'Ted Zlatanov' and UID/GID '10000/20000', living in '/home/tzz' with shell '/bin/bash' and password hash 'mypass'.  The group name is mygroup.|,
+            "test user info2" => qr|Requested user tzz2 with GECOS 'Ted Zlatanov 2' and UID/GID '10001/20001', living in '/home/tzz2' with shell '/bin/bash' and password hash 'mypass2'.  The group name is mygroup.|,
+            "test user info2" => qr|pwentry for tzz is 'tzz:x:10000:20000:Ted Zlatanov:/home/tzz:/bin/bash|,
             "test user info3" => qr|groupentry for tzz is 'mygroup:x:20000:|,
             "test user info4" => qr|shadowentry for tzz is 'tzz:mypass:\d+:0:99999:7:::|,
+            # let's assume there is no tzz2 user in the world, OK?
+            "runbased" => qr|tzz2 was missing|,
+            "runbased2" => qr|/usr/sbin/useradd -d /home/tzz2 -c Ted Zlatanov 2 -g 20001 -m -p mypass2 -s /bin/bash -u 10001 tzz2|,
            };
 
 my $output = dctest::setup('./test.cf', $todo);
