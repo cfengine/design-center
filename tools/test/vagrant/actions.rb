8aa63926ceb37fae3493d81ef088d93799f22102
Ted Zlatanov <tzz@lifelogs.com>
2013-07-08 12:20:26 -0400

    tools/test/vagrant: add --install_version=X option

--- tools/test/vagrant/actions.rb
+++ tools/test/vagrant/actions.rb
@@ -1,13 +1,15 @@
 module Actions
 
-  def Actions.assemble(type, installer, setup_steps, dctest)
+  def Actions.assemble(type, installer, version, setup_steps, dctest)
     steps = []
 
     # pp method(__method__).parameters.map{|arg| __method__.to_s + " #{arg[1]} = #{eval arg[1].to_s}" }
 
     # note order: install, then bootstrap, then install DC, then other steps
+    installer_args = []
+    installer_args << version if version
 
-    steps << { :path => "install_#{type}_#{installer}.sh", :args => [] }
+    steps << { :path => "install_#{type}_#{installer}.sh", :args => installer_args }
 
     unless dctest.empty?
       setup_steps += [ 'dc' ]
--- tools/test/vagrant/resources/shell_provisioners/install_ubuntu_packages.sh
+++ tools/test/vagrant/resources/shell_provisioners/install_ubuntu_packages.sh
@@ -4,13 +4,24 @@
 test -d /var/cfengine && exit 0
 
 cd /tmp
-wget -q http://cfengine.com/pub/gpg.key
-apt-key add gpg.key
-rm gpg.key
 
-apt-get -qq update
-apt-get -qq -y install python-software-properties
-apt-get -qq -y install software-properties-common
-add-apt-repository http://cfengine.com/pub/apt
-apt-get -qq update
-apt-get -qq -y install cfengine-community
+if [ -z "$1" ]; then
+    echo Installing Community packages
+
+    wget -q http://cfengine.com/pub/gpg.key
+    apt-key add gpg.key
+    rm gpg.key
+
+    apt-get -qq update
+    apt-get -qq -y install python-software-properties
+    apt-get -qq -y install software-properties-common
+    add-apt-repository http://cfengine.com/pub/apt
+    apt-get -qq update
+    apt-get -qq -y install cfengine-community
+else
+    while [ -n "$1" ]; do
+        echo Installing requested package $1
+        /usr/bin/dpkg -i $1
+        shift
+    done
+fi
