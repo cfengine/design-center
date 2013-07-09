245bcf97185117722a9e9dd41fde690f2ad2ba7a
Ted Zlatanov <tzz@lifelogs.com>
2013-07-09 11:30:21 -0400

    tools/test/vagrant: For Ubuntu, fall back to a hard-coded package if the APT repo is not usable

--- tools/test/vagrant/resources/shell_provisioners/install_ubuntu_packages.sh
+++ tools/test/vagrant/resources/shell_provisioners/install_ubuntu_packages.sh
@@ -15,9 +15,15 @@ if [ -z "$1" ]; then
     apt-get -qq update
     apt-get -qq -y install python-software-properties
     apt-get -qq -y install software-properties-common
-    add-apt-repository http://cfengine.com/pub/apt
-    apt-get -qq update
-    apt-get -qq -y install cfengine-community
+
+    if add-apt-repository http://cfengine.com/pub/apt; then
+        apt-get -qq update
+        apt-get -qq -y install cfengine-community
+    else
+        echo Could not add the APT repository, falling back to try i386 and amd64 packages
+        /usr/bin/dpkg -i /vagrant/resources/packages/cfengine-community_3.5.0-1_i386.deb
+        /usr/bin/dpkg -i /vagrant/resources/packages/cfengine-community_3.5.0-1_amd64.deb
+    fi
 else
     while [ -n "$1" ]; do
         echo Installing requested package $1
