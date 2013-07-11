d8bbb3cff7ef95730ea5150caa0bbb349919c7d0
Ted Zlatanov <tzz@lifelogs.com>
2013-07-11 12:56:52 -0400

    tools/test/vagrant: add masterfiles archive and extract it with setup_masterfiles.sh

--- tools/test/vagrant/resources/shell_provisioners/setup_masterfiles.sh
+++ tools/test/vagrant/resources/shell_provisioners/setup_masterfiles.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -x
 
-ORIGIN=/vagrant/resources/masterfiles.git
+ORIGIN=/vagrant/resources/masterfiles.git.tgz
 REPO=/var/tmp/masterfiles.git
 
 # If $REPO exists, do nothing, assume it's installed already
 test -d $REPO && exit 0
 
-sudo -u apache git clone --mirror $ORIGIN $REPO
+cd `dirname $REPO`
+sudo -u apache tar -xzf $ORIGIN
 
 # exit status = test status
 test -d $REPO
