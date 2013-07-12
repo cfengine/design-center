93a3b3b6d72ba721c3d6b93847a787fef4933242
Ted Zlatanov <tzz@lifelogs.com>
2013-07-12 10:50:48 -0400

    tools/test/vagrant: moving to https://github.com/tzz/vagrant-cfe

--- tools/test/vagrant/resources/shell_provisioners/bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-
-echo $1 > /var/tmp/policy_hub
-# try the new bootstrap syntax and then the 3.0.x syntax
-/var/cfengine/bin/cf-agent --bootstrap=$1 || /var/cfengine/bin/cf-agent --bootstrap --policy-server=$1
