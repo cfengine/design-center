79f74bb2181f0d6d7aa9145ebd1cd2eb714d234d
Aleksey Tsalolikhin <aleksey@verticalsysadmin.com>
2013-10-26 11:53:54 -0700

    add awareness of Debian pkg system; improve colors to make mistakes stand out more

--- examples/verticalsysadmin_training_examples/l.sh
+++ examples/verticalsysadmin_training_examples/l.sh
@@ -1,10 +1,33 @@
 #!/bin/sh
-# colorize "ls" output
+
+# colorize "ls" output to read the table of contents for the course materials.  
+# differentiate chapter numberting from section numbering from section titles;
+# highlight in red file names that do not match expected pattern.
+# Aleksey Tsalolikhin
+
+install_remark() {
+  echo
+  echo
+  echo Installing regex-markup
+
+  if [ -f /bin/rpm ]
+  then
+          sh -x -c 'wget http://download.savannah.gnu.org/releases/regex-markup/regex-markup-0.10.0-1.x86_64.rpm' && sudo rpm -ihv ./regex-markup-0.10.0-1.x86_64.rpm
+  fi
+
+  if [ -f /usr/bin/dpkg ]
+  then
+	  echo got this far
+	  sh -x -c 'wget  http://gnu.mirrors.pair.com/savannah/savannah/regex-markup/regex-markup_0.10.0-1_amd64.deb' && sudo dpkg -i ./regex-markup_0.10.0-1_amd64.deb
+  fi
+
+  echo press ENTER to continue; read A
+}
 
 
 # l.sh depends on "remark" which is provided by "regex-markup" from nongnu.org.
 # let's check that "remark" is installed, and if it's not, then install it
-remark /dev/null /dev/null >/dev/null 2>/dev/null || (echo; echo; echo Installing regex-markup RPM; sh -x -c 'wget http://download.savannah.gnu.org/releases/regex-markup/regex-markup-0.10.0-1.x86_64.rpm' && rpm -ihv ./regex-markup-0.10.0-1.x86_64.rpm; echo press ENTER to continue; read A)
+remark /dev/null /dev/null >/dev/null 2>/dev/null || install_remark
 
 
 # display a colorized file list, in alphanumeric order
